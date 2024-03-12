edd79be56c9cbee076aa3d422d1aeade2d05ba70
Willbemax <max22042007@yandex.ru>
2024-03-12 21:21:16 +0300

    Добавленны задачи из трнировок алгоритмов

--- YandexContest/chessboard/solution.js
+++ YandexContest/chessboard/solution.js
@@ -1,4 +1,49 @@
 //node.js
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter the value of N: ', (N) => {
+    let cells = [];
+    let perimeter = 0;
+
+    const is_cut_cell = (row, col) => {
+        return cells.some(cell => cell[0] === row && cell[1] === col);
+    };
+
+    const check_neighbors = (row, col) => {
+        let count = 0;
+        if (cells.some(cell => cell[0] === row - 1 && cell[1] === col)) {
+            count += 1;
+        }
+        if (cells.some(cell => cell[0] === row + 1 && cell[1] === col)) {
+            count += 1;
+        }
+        if (cells.some(cell => cell[0] === row && cell[1] === col - 1)) {
+            count += 1;
+        }
+        if (cells.some(cell => cell[0] === row && cell[1] === col + 1)) {
+            count += 1;
+        }
+        return count;
+    };
+
+    rl.question('Enter the cells: ', (input) => {
+        cells = input.split(' ').map(cell => cell.split(',').map(Number));
+
+        cells.forEach(cell => {
+            const [row, col] = cell;
+            perimeter += 4 - check_neighbors(row, col);
+        });
+
+        console.log(perimeter);
+        rl.close();
+    });
+});
+
 
 
 //python
