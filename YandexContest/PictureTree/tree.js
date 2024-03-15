1fd70c7e3314f358988c0230eb83b45a298a8de0
Willbemax <max22042007@yandex.ru>
2024-03-15 23:18:27 +0300

    добавленною решение задачи на покрас деревьев

--- YandexContest/PictureTree/tree.js
+++ YandexContest/PictureTree/tree.js
@@ -22,3 +22,12 @@ rl.question('', (input) => {
 //
 // result = 2 * (v + m + 1) - max(0, min(p + v, q + m) - max(p - v, q - m) + 1)
 // print(result)
+
+//p, v = map(int, input().split())
+// q, m = map(int, input().split())
+// minv, maxv = p - v, p + v
+// minm, maxm = q - m, q + m
+// if max(minv, minm) <= min(maxv, maxm):
+//     print(max(maxv, maxm) - min(minv, minm) + 1)
+// else:
+//     print((maxv - minv + 1) + (maxm - minm + 1))
