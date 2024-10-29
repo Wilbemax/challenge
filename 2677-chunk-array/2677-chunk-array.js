628e476992679a59650a90fe3c414f031211ae49
Maksim <95708730+Wilbemax@users.noreply.github.com>
2024-10-29 22:26:16 +0300

    Time: 64 ms (18.59%), Space: 52.1 MB (20.22%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    var nums  = []
+    
+    for(var i = 0; i < arr.length; i +=size ){
+        nums.push(arr.slice(i, i+ size))
+    }
+    
+    
+   return nums
+};
