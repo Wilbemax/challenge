f7eade7041864f6d628d032d269dffe3e9ff6f2d
Maksim <95708730+Wilbemax@users.noreply.github.com>
2024-10-29 22:18:26 +0300

    Time: 185 ms (48.00%), Space: 79.3 MB (46.21%) - LeetHub

---
+++ 2649-nested-array-generator/2649-nested-array-generator.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+
+
+
+var inorderTraversal = function* (arr) {
+   for (item of arr) {
+    if (Array.isArray(item)){
+        yield* inorderTraversal(item)
+    }else{
+        yield item
+    }
+   }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
