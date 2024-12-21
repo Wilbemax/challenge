8c1be6ea2ce2d627e929052e5a0a2391b2de97aa
Maksim <95708730+Wilbemax@users.noreply.github.com>
2024-12-21 21:51:07 +0300

    Time: 93 ms (94.74%), Space: 83 MB (60.53%) - LeetHub

---
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} values
+ * @param {number} k
+ * @return {number}
+ */
+var maxKDivisibleComponents = function(n, edges, values, k) {
+    
+    const adj = Array.from({length : n}, () => new Set());
+
+    for(const [u, v] of edges) {
+        adj[u].add(v);
+        adj[v].add(u);
+    }
+
+    let components = 0;
+
+    const dfs = (node, parent = null) => {
+
+        let sum = values[node];
+
+        for(const u of adj[node]) {
+
+            if(u === parent) continue;
+            
+            sum += dfs(u, node);
+        }
+
+        components += sum % k === 0 ? 1 : 0;
+
+        return sum;
+    }
+
+    dfs(0);
+    
+    return components;
+}
+  
